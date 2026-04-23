528e984b59a851270b4f129287ed5eb5103b408c
Diego Armando Gutiérrez Ayala <diegogtz1207@gmail.com>
2026-04-23 02:36:53 -0600

    remove testing comment
    Removed a testing comment from the getBooks function.

--- demo_api/lambdas/get-books/src/handler.ts
+++ demo_api/lambdas/get-books/src/handler.ts
@@ -4,6 +4,5 @@ import { successResponse } from "@demo-api/api-common";
 
 export const getBooks = async (_event: APIGatewayProxyEvent) => {
   const books = await booksRepository.getAll();
-  // this is a testing comment
   return successResponse({ books, count: books.length });
 };
