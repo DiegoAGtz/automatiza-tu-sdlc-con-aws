084694a8a1576d97671c67b8028a1ef7383022be
Diego Armando Gutiérrez Ayala <diegogtz1207@gmail.com>
2026-04-23 03:58:25 -0600

    Add comment to getBooks function
    Added a comment to the getBooks function.

--- demo_api/lambdas/get-books/src/handler.ts
+++ demo_api/lambdas/get-books/src/handler.ts
@@ -3,6 +3,7 @@ import { booksRepository } from "@demo-api/db-repositories";
 import { successResponse } from "@demo-api/api-common";
 
 export const getBooks = async (_event: APIGatewayProxyEvent) => {
+  // hello lambda
   const books = await booksRepository.getAll();
   return successResponse({ books, count: books.length });
 };
