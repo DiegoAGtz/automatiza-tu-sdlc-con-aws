e1623d89e82cd0930fd45570f894de6b8414e1ff
Diego Armando Gutiérrez Ayala <diegogtz1207@gmail.com>
2026-04-23 02:32:35 -0600

    Add testing comment in getBooks handler
    Added a comment for testing purposes.

--- demo_api/lambdas/get-books/src/handler.ts
+++ demo_api/lambdas/get-books/src/handler.ts
@@ -4,5 +4,6 @@ import { successResponse } from "@demo-api/api-common";
 
 export const getBooks = async (_event: APIGatewayProxyEvent) => {
   const books = await booksRepository.getAll();
+  // this is a testing comment
   return successResponse({ books, count: books.length });
 };
